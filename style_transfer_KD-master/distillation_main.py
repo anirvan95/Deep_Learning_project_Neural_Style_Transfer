7f47adeada77ce387701ff332f8a5afe67e94203
Anirvan Dutta <anirvan.dutta95@gmail.com>
2019-05-19 23:35:37 +0200

    Removed duplicate folder

--- style_transfer_KD-master/distillation_main.py
+++
@@ -1,4 +0,0 @@
-from knowledge_distillation.distill import main
-
-if __name__ == '__main__':
-    main()
--- style_transfer_KD-master/run_stilisation.sh
+++
@@ -1,8 +0,0 @@
-python neural_style/neural_style.py eval \
-  --content-image images/content-images/amber.jpg \
-  --model distillation_results/checkpoint_0.tar \
-  --output-image images/output-images/test2.jpg \
-  --cuda 1 \
-  --distilled
-  
-#--model saved_models/mosaic.pth \
